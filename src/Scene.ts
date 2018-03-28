3310b604ed2db3262f36a796e39f0ebad699599b
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-03-28 16:04:48 -0300

    Criação das classe

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,14 +1,15 @@
-<reference path = "ArrayList"/>
+/// <reference path = "ArrayList.ts" />
 
 class Scenes{
   private id: number
   private title: string
   private description: string
-  private object: rrayList
+  private object: ArrayList
 
-  constructor (){
-
-
-      }
-
+  constructor (id:number, title: string, description: string, object: ArrayList){
+    this.id = id
+    this.title = title
+    this.description = description
+    this.object = object
+    }
 }
