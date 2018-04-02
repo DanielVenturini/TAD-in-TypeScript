4bee128a9370e6c010b7cec03abfa14ae0374471
Daniel Venturini <daniellagarto0@gmail.com>
2018-04-02 09:02:08 -0300

    Transformando arquivo JSON em Object

--- src/Game.ts
+++ src/Game.ts
@@ -9,5 +9,4 @@ class Game{
     this.currentScene = 0
   }
 
-  
 }
--- src/Scene.ts
+++ src/Scene.ts
@@ -12,4 +12,36 @@ class Scenes{
     this.description = description
     this.object = object
     }
+
+  getId():number{
+    return this.id
+  }
+
+  setId(id:number){
+    this.id = id
+  }
+
+  getTitle():string{
+    return this.title
+  }
+
+  setTitle(title:string){
+    this.title = title
+  }
+
+  getDescription():string{
+    return this.description
+  }
+
+  setDescription(description:string){
+    this.description = description
+  }
+
+  getObjects():ArrayList{
+    return this.object
+  }
+
+  setObjects(object:ArrayList){
+    this.object = object
+  }
 }
