01013ed0ff193915d8b1eff1012fc600f41dc4ff
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-04-16 00:08:59 -0300

    go in

--- src/Game.ts
+++ src/Game.ts
@@ -3,21 +3,15 @@
 var readline = require('readline-sync');
 var fs = require('fs');
 
-
-
 class Game{
   scenes: 
-  objects: 
-  currentScene: number
-   
 
+
+   
   constructor(){
 
     var game = JSON.parse(fs.readFileSync('./src/game.json', 'utf8'));
     this.scenes = game.cenas
-    this.objects = game.objects
-    this.currentScene = game.cenaAtual
-
 
     
   }
--- src/Main.ts
+++ src/Main.ts
@@ -20,14 +20,14 @@ var start = true
 var start2 = true
 while(start){
 
-    console.log("\n\n\n" + cenas[i].description)
-
+    console.log("\n\n" + cenas[i].description)
+    start2 = true
     // dois while, um para interagir na cena, e outro para carregar a outra cena
     while(start2){
         var comandoUsuario = rl.prompt(">");
 
         var comando = comandoUsuario.toLowerCase().split(" ")
-        console.log(comando)
+       // console.log(comando)
         switch(comando[0]){
             case "exit": {
                 start = false
@@ -36,15 +36,47 @@ while(start){
             }
 
             case "use": {
-                if(validacaoObjetoInventario() == false){
-                    console.log("Comando inválido. Por favor, tente novamente")
-                    continue
+                if(comando[2] != "with"){
+                    if(comandoUsuario == "use tocha" && i == 4){//destrava caminho
+                        for(let pos in objetos){ 
+                            if(objetos[pos].name == comando[1]){
+                                //console.log(objetos[pos])
+                                cenas[i].resolved[2] = true
+                                console.log(objetos[pos].txtPositivo[0])
+                                break
+                            }
+                        }
+                    }
+                    else{
+                    console.log("comando errado")            
+                    }
                 }
-
+                
+                    if(validacaoObjetoInventario() == false){
+                        console.log("Comando inválido. Por favor, tente novamente")
+                    }
                 break
             }
 
             case "go": {
+                
+                
+                if( (comando[2] == "saida1" || comando[2] == "saida") && cenas[i].resolved[0] == true){
+                    i = cenas[i].exit[0]
+                    start2 = false
+                }
+                
+                else if(comando[2] == "saida2" && cenas[i].resolved[1] == true){
+                    i = cenas[i].exit[1]
+                    start2 = false
+                }
+
+                else if(comando[2] == "saida3" && cenas[i].resolved[2] == true){
+                    i = cenas[i].exit[2]
+                    start2 = false
+                }
+            
+
                 break
             }
 
@@ -56,36 +88,38 @@ while(start){
 
                 let object = comando[1]
                 let resposta = 'Comando inválido.'
+                let encontrado = false
 
-                console.log(inventario)
+               // console.log(inventario)
                 for(let posObj in inventario){          // procurando objeto no inventario
                     if(inventario[posObj] == object){   // se ja estiver
                         resposta = 'Objeto ' + object + ' já está no inventário.'
+                        encontrado = true               
                         break
                     }
                 }
 
-                for(let posObj in cenas[i].object){     // procurando nos objetos da cena
-                    if(cenas[i].object[posObj] == object){
-                        if(cenas[i].object[posObj].got == -1){
-                            resposta = "Nao é possivel pegar o " + object
+                if(encontrado != true){
+                    for(let posObj in cenas[i].object){     // procurando nos objetos da cena
+                        if(cenas[i].object[posObj] == object){
+                            if(cenas[i].object[posObj].got == -1){
+                                resposta = "Nao é possivel pegar o " + object
+                                break
+                            }
+                            cenas[i].object[posObj].got = 1//errad0
+                            console.log("---------" + objetos[4].got)
+                            inventario.push(object)
+                            resposta = 'Objeto ' + object + ' agora está no inventário.'
                             break
                         }
-
-                        cenas[i].object[posObj].got = 1
-                        inventario.push(object)
-                        resposta = 'Objeto ' + object + ' agora está no inventário.'
-                        break
                     }
                 }
-
+                encontrado = false
                 console.log(resposta)
+                if(comando[1] == "jarro"){console.log(objetos[4].txtPositivo)}// trata o jarro
             }
         }
     }
-
-    i 
-
 }
 
 
